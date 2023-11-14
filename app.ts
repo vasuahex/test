e3feb606c6db632c29c38cc09a34ae9250170b57
vasu <knsrinivasareddy@gmail.com>
2023-11-14 11:33:14 +0530

    object code added

--- app.ts
+++ app.ts
@@ -1,4 +1,35 @@
-console.log("vasu ahex");
-
-const vasu = ["vasu ahex"]
-const venu=["a","b"]
+// Define a User interface
+interface User {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }
+  
+  // Function to generate a random ID
+  function generateRandomId(): number {
+    return Math.floor(Math.random() * 1000);
+  }
+  
+  // Array to store user objects
+  let users: User[] = [];
+  
+  // Function to create a user object
+  function createUserObject(name: string, age: number, email: string): User {
+    return {
+      id: generateRandomId(),
+      name: name,
+      age: age,
+      email: email,
+    };
+  }
+  
+  // Generate 10 user objects and add them to the array
+  for (let i = 0; i < 10; i++) {
+    let user: User = createUserObject(`User${i + 1}`, 20 + i, `user${i + 1}@example.com`);
+    users.push(user);
+  }
+  
+  // Print the array of user objects
+  console.log(users);
+  
